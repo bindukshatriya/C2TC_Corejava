69193a6237f53d1ecb5062e95c67e97cad3becfe
BINDU <BINDU@BINDU>
2022-05-23 14:22:29 +0530

    FIRST COMMIT

---
+++ C2TC_TRAINING/src/threadpg/DAY1_P1.java
@@ -0,0 +1,15 @@
+package threadpg;
+class withdraw
+{
+	  int amt_withdraw=2000;
+}
+
+public class DAY1_P1 extends withdraw{
+	int amt_left=5000;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      DAY1_P1 ob1=new DAY1_P1();
+      System.out.println("amount withdraw"+ob1.amt_withdraw);
+	}
+
+}
---
+++ C2TC_TRAINING/src/threadpg/DAY1_P2.java
@@ -0,0 +1,25 @@
+package threadpg;
+
+public class DAY1_P2 {
+	int year_of_pur;
+	String make;
+	int model;
+	double cost;
+	public void disp1()
+	{
+		System.out.println("year of purchase"+20001);
+		System.out.println("make"+"suzuki");
+	}
+	public void disp2()
+	{
+		System.out.println("model"+20001);
+		System.out.println("make"+"suzuki");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         DAY1_P2 ob=new DAY1_P2();
+         DAY1_P2 ob1=new DAY1_P2();
+         ob.disp1();
+         ob.disp2();
+	}
+}
